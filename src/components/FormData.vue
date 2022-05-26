ccc652f4e0711948bdb928c8194d7333917ab5bd
rafhaelcardoso <rafhaelcardosos@gmail.com>
2022-05-25 23:49:36 -0300

    feat: :art: Add FormData component to the project
    Add FormData component to the project

---
+++ src/components/FormData.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para a criação de uma nova tarefa!"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar ?"
+        />
+      </div>
+      <div class="columns">
+        <div class="is-flex is-align-item-center is-justify-content-space-around  is-align-items-center">
+          <section class="mx-2">
+            <strong> 00:00:00 </strong>
+          </section>
+          <button class="button mx-2">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button mx-2">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormData",
+});
+</script>
+
+<style scoped></style>
