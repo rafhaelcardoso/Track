5d114f17e0e25f2a82746d139eb3ac7d68372f7a
rafhaelcardoso <rafhaelcardosos@gmail.com>
2022-05-26 08:10:55 -0300

    feat: :sparkles: Add play and stop methods
    Add play and stop methods

--- src/components/FormData.vue
+++ src/components/FormData.vue
@@ -15,15 +15,15 @@
       <div class="columns">
         <div class="is-flex is-align-item-center is-justify-content-space-around  is-align-items-center">
           <section class="mx-2">
-            <strong> 00:00:00 </strong>
+            <strong> {{ elapsedTime }} </strong>
           </section>
-          <button class="button mx-2">
+          <button class="button mx-2" @click="play">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button mx-2">
+          <button class="button mx-2" @click="stop">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -40,6 +40,27 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormData",
+  data (){
+    return{
+      timeInSeconds: 0
+    }
+  },
+  computed: {
+    elapsedTime() : string {
+      return new Date(this.timeInSeconds * 1000).toISOString().substr(11,8)
+    }
+  },
+  methods : {
+    play(){
+      setInterval(()=>{
+      this.timeInSeconds +=1
+      }, 1000)
+    },
+
+    stop(){
+      clearInterval(this.timeInSeconds)
+    },
+  }
 });
 </script>
 
